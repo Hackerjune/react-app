e86914d3e2642fef5d90c618bc67ac0ba426f3e4
Hackerjun <psn_0125@naver.com>
2024-01-25 19:47:07 +0900

    리액트 기초

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header(props){
+console.log('props', props, props.title);
+return <header>
+        <h1><a href='/'>{props.title}</a></h1>   
+      </header>
+}
+
+function Nav(){
+  return  <nav>
+  <ol>
+    <li><a href='/'>html</a></li>
+    <li><a href='/'>css</a></li>
+    <li><a href='/'>js</a></li>
+  </ol>
+</nav>
+}
+
+
+function Article(props){
+  console.log('props', props, props.title, props.body);
+ return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="REACT"></Header>
+      <Nav></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
