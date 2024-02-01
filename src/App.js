12cb4560015fa4321f8b859be1741129c3e833ba
Hackerjun <psn_0125@naver.com>
2024-02-01 18:49:43 +0900

    props

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ function App() {
 
   let [글제목, setTitle] = useState(['남자 코트 추천', '파이썬 독학', '우동 맛집']);    // 글제목은 변수, b는 변경용 함수 state는 변수가 변경되면 바로 렌더링 됨 
 
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
 
   let [modal, setModal] = useState(false);
+
+  
 
   // let [글제목2, setTitle2] = useState('우동 맛집');
 
   // let [글제목3, setTitle3] = useState('파이썬독학');
+
+  // [1,2,3].map(function(it){     // map 함수에서 매개변수는 상위 배열의 자료를 호출한다
+  //   return 123                  // map 함수 => 상위 배열의 자료 갯수만큼 리턴값 호출, 호출한 값으로 새로운 배열 생성
+  // })
 
   let 발행일 = '2024-1-29'
 
@@ -34,24 +40,31 @@ function App() {
         copy[0] = '여자 코트 추천';        // 원본 보존형 코딩
         setTitle(copy); } }>↺</span> 
 
-      <div className='list'> 
-        <h4 onClick={ ()=>{ setModal(!modal) } }>{글제목[0]}</h4> <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} 
-        <p>{발행일}</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={ ()=>{ setModal(!modal) } }>{글제목[1]}</h4>
-        <p>{발행일}</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={ ()=>{ setModal(!modal) } }>{글제목[2]}</h4>
-        <p>{발행일}</p> 
-      </div>
-
       
+        {
+          글제목.map(function(a, i){             // i는 반복 실행 될때마다 1씩 증가함
+            return ( <div className='list'>
+                    <h4 onClick={ ()=>{ setModal(!modal)
+                     
+                      } } >{글제목[i]} </h4> 
+
+                    <span onClick={ () => { 
+                      let copy따봉 = [...따봉];
+                      따봉변경(copy따봉);
+                      copy따봉[i] = copy따봉[i] + 1;
+                     } }>👍</span> {따봉[i]} 
+
+                    <p>{발행일}</p>
+                  </div> )
+          })
+        }
+
+
 
 {
-  modal === true ?  <Modal title='제목' date={발행일} inpo='상세정보' className='modal1'></Modal> : null
-}
+  modal === true ?  <Modal  setTitleFunction={setTitle} setTitle={글제목} title={글제목[0]} date={발행일} inpo='상세정보' className='modal1'></Modal> : null
+} 
+
      
     </div>
   );
@@ -63,6 +76,11 @@ function Modal(props){
       <h4>{props.title}</h4>
       <p>{props.date}</p>
       <p>{props.inpo}</p>
+      <button onClick={ function(){
+        let modal글제목 = [...props.setTitle]
+        modal글제목[0] = "여자 코트 추천"
+        props.setTitleFunction(modal글제목)
+      } }>↺</button>
     </div>
   )
 }
